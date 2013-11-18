6ca1b7e4ea832bb6a9b926bfc80f762e29f40c9a
Julien Valéry <nodejsaddict@gmail.com>
2013-11-18 16:04:51 -0500

    added new files

---
+++ server.js
@@ -0,0 +1,65 @@
+// reference the http module so we can create a webserver
+var http = require("http");
+var path = require('path');
+var twitter = require("./twitter.js");
+var express = require('./node_modules/express/index.js');
+
+//
+var options = {authKey:'bzJZSlN4ZnJUYWhyeXdub2R4MzJBOkFHSmw5MnJIeEFTRkpYVW9BSm8zMEpTQzU2Wm0zNFZxZmFVZFh1TUZWamc='};
+
+var init = false; 
+
+var app = express();
+//exports.app = app;
+
+app.configure(function(){
+  //app.set('port', process.env.PORT || 4000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.set('view cache', true);
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());  
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.get('/', function(req, res){ 
+    res.render('test');
+});
+
+app.get('/search', function(req, res){ 
+	var hash = req.query.hashtag;
+    twitter.query({hashtag:hash}, function(err, data) {
+        res.writeHead(200, {"Content-Type": "application/json"});
+        var json = JSON.stringify(data);
+        res.end(json);
+    });
+});
+
+app.listen(8080, function(){
+	if (!init) {
+        twitter.auth(options, function(err, data) {
+          init=true;
+        
+        });
+    }
+});
+
+// create a server
+/*http.createServer(function(req, res) {
+    // on every request, we'll output 'Hello world'
+    res.end("Hello world from Cloud9!");
+    
+    console.log(req.url);
+    
+}).listen(process.env.PORT, process.env.IP);*/
+
+
+
+// Note: when spawning a server on Cloud9 IDE, 
+// listen on the process.env.PORT and process.env.IP environment variables
+
+// Click the 'Run' button at the top to start your server,
+// then click the URL that is emitted to the Output tab of the console
---
+++ twitter.js
@@ -0,0 +1,76 @@
+// node http api
+var http = require('http');
+var OAuth = require('./node_modules/oauth/index.js');
+var OAuth2 = OAuth.OAuth2;  
+
+var twitterConsumerKey = 'o2YJSxfrTahrywnodx32A';
+ var twitterConsumerSecret = 'AGJl92rHxASFJXUoAJo30JSC56Zm34VqfaUdXuMFVjg';
+ var oauth2 = new OAuth2(
+   twitterConsumerKey,
+   twitterConsumerSecret, 
+   'https://api.twitter.com/', 
+   null,
+   'oauth2/token', 
+   null);
+
+var bearer;
+
+function Twitter() {
+  this.init();
+}
+
+Twitter.prototype.init = function() {
+  return this;
+};
+
+Twitter.prototype.auth = function(options, next) {
+    oauth2.getOAuthAccessToken(
+       '',
+       {'grant_type':'client_credentials'},
+       function (e, access_token, refresh_token, results){
+         console.log('bearer: ',access_token);
+         bearer = access_token;
+         return next(e, access_token);
+    });
+    
+  return this;
+};
+
+Twitter.prototype.query = function(options, next) {
+    
+    var paramSince = options.since ? '&since_id='+ options.since : '';
+	var queryUrl = 'https://api.twitter.com/1.1/search/tweets.json?q=%'+options.hashtag+paramSince;
+	
+	console.log(queryUrl);
+	console.log(bearer);
+	
+	oauth2.useAuthorizationHeaderforGET(true);
+	
+    oauth2.get(queryUrl, bearer, function(e,data, response) {
+         //console.log('e'+JSON.stringify(e));
+         if (e) {
+             //console.log('error');
+             return next(e, null);
+         }
+         //console.log(res.statusCode);
+         /*if (res.statusCode!=200) 
+           return next(new Error(
+             'OAuth2 request failed: '+
+             res.statusCode),null);*/
+         try {
+            //console.log(data);
+           data = JSON.parse(data);        
+         }
+         catch (e){
+           return next(e, null);
+         }
+         return next(e, data);
+      });
+      
+      //console.log('bearer' + bearer);
+    
+    return this;
+};
+
+var twitter = new Twitter();
+module.exports = twitter;
