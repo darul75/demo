b1dba5f6a6d251cc1042976a0a353e77bfaf9676
darul <darul75@gmail.com>
2014-01-12 22:36:25 +0100

    planetary demo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // reference the http module so we can create a webserver
 var http = require("http");
 var path = require('path');
+var events = require('events');
 var twitter = require("./twitter.js");
 var express = require('express');
 var satelize = require('satelize');
@@ -11,6 +12,87 @@ var captureweb = require('captureweb');
 var base64encode = require('base64-stream').Encode;
 var fs = require('fs');
 var xml2js = require('xml2js');
+var topojson = require('topojson');
+var twitter2 = require('ntwitter');
+var cronJob = require('cron').CronJob;
+
+var eventEmitter = new events.EventEmitter();
+
+var twitterConsumerKey = 'o2YJSxfrTahrywnodx32A';
+var twitterConsumerSecret = 'AGJl92rHxASFJXUoAJo30JSC56Zm34VqfaUdXuMFVjg';
+var access_token_key = '53659570-doYYVH7QT9Xmcqq7nmtRG8p5yq9nH4Zuruw28cBbT';
+var access_token_secret = 'QVK8dddxacH0327lnqsjC7iqfMlIpyxg9pkE1Aa5w9Oef';
+
+var twit = new twitter2({
+  consumer_key: twitterConsumerKey,
+  consumer_secret: twitterConsumerSecret,
+  access_token_key: access_token_key,
+  access_token_secret: access_token_secret
+});
+
+var featuresTweets = [];
+
+var newTweet = function newTweet(tweet)
+{  
+
+  var o = {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": tweet.coordinates.coordinates
+      },
+      "properties": {
+        "text": tweet.text,
+        "lng": tweet.coordinates.coordinates[0],
+        "lat": tweet.coordinates.coordinates[1]
+      }
+  };
+
+  featuresTweets.push(o);
+
+  if (featuresTweets.length % 10 === 0) {      
+      outputFile = __dirname+'/public/tweets.json';
+      var newArray = JSON.parse(JSON.stringify(featuresTweets));
+      var collection = {type: "FeatureCollection", features: featuresTweets};
+      var topology = topojson.topology({collection: collection}, {"property-transform":function propertyTransform(properties, key, value) {
+          properties[key] = value;
+          return true;
+        }
+      }); // convert to TopoJSON
+
+      featuresTweets = newArray;
+
+      //console.log(JSON.stringify(featuresTweets));
+
+      // var json = JSON.stringify(collection);
+      app.set("tweetJSON", JSON.stringify(topology));
+      // fs.writeFile(outputFile, JSON.stringify(topology), function (err) {
+      //   //console.log(writed);
+      //   //fs.writeFileSync(outputFile, JSON.stringify(topology));
+      // });
+    }
+}
+
+twit.stream('statuses/filter', {'track':'football'}, function(stream) {
+  stream.on('data', function (data) {
+    if (data.geo)
+      eventEmitter.emit('newTweet', data);
+  });
+});
+
+eventEmitter.on('newTweet', newTweet);
+
+// SMALL CRON
+var cronJob = require('cron').CronJob;
+var job = new cronJob({
+  cronTime: '00 00 00 * * 1-7',
+  onTick: function() {
+    app.set("tweetJSON", JSON.stringify({result:'nok'}));
+  },
+  start: false,
+  timeZone: "America/Los_Angeles"
+});
+job.start();
 
 var parser = new xml2js.Parser
 var useragents = '';
@@ -79,6 +161,14 @@ app.get('/starter', function(req, res){
 
 app.get('/dynupdate', function(req, res){ 
     res.render('dynupdate');
+});
+
+app.get('/planetary-twitter', function(req, res){ 
+    res.render('planetary-twitter');
+});
+
+app.get('/twitter', function(req, res) {
+  res.end(app.get("tweetJSON"));
 });
 
 app.post('/capturewebquery', function(req, res){ 
@@ -200,6 +290,7 @@ app.get(function(req, res){
 /*app.listen(8080, function(){*/
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
+  app.set("tweetJSON", JSON.stringify({result:'nok'}));
 	if (!init) {
         twitter.auth(options, function(err, data) {
           init=true;
