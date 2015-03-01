581df83a0d39590ba026c21772f4e8af0bb09b68
julien <darul75@gmail.com>
2015-03-01 21:09:38 +0100

    l

--- test.js
+++ test.js
@@ -1,110 +1,97 @@
-/*
+var readline = require('./utils.js'),
+      rl = readline('./mower.txt');
 
-5 5
-1 2 N
-GAGAGAGAA
-3 3 E
-AADAADADDA
 
-RESULT
 
-1 3 N
-5 1 E
+var Cardinals = {
+  'N': {x:0, y:1},
+  'E': {x:1, y:0},
+  'W': {x:-1, y:0},
+  'S': {x:0, y:-1}
+};
 
-*/
-
-var N= {x:0, y:1};
-var E= {x:1, y:0};
-var W= {x:-1, y:0};
-var S= {x:0, y:-1};
-
-var CARDINALS = [N,E,S,W];
 var CARDINALS_STR = ['N','E','S','W'];
 
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
 
-var Vehicule = { x: 0, y : 0, o: N};
-var Vehicule2 = { x: 0, y : 0, o: N};
+function Mower(x, y, direction) {  
+  this.position = new Vector(x,y);
+  this.direction = direction;
+  this.directionVector = Cardinals[direction];
+}
 
-var Area = { x: 0, y: 0}; // limits
+Mower.prototype.handleCommand = function(instructions) {
+  instructions.split('').forEach(function(flag) {
+    switch (flag) {
+      case 'G':
+      case 'D':
+        var currentPos = this.direction;
+        this.prevDirection = CARDINALS_STR.indexOf(currentPos);
+        var newIdx = this.prevDirection - 1 >= 0 ? this.prevDirection - 1 : CARDINALS_STR.length -1;
+        if (flag === 'D'){
+          newIdx = this.prevDirection + 1 > 3 ?  0 : this.prevDirection + 1;
+        }
+        this.direction = CARDINALS_STR[newIdx];
+      break;    
+      default:
+        this.move();
+      break;     
+    }     
+  }, this);  
+
+  return {
+    x:this.position.x,
+    y:this.position.y,
+    direction: this.direction
+  }  
+};
+
+Mower.prototype.move = function() { 
+  var CARD_RULE = Cardinals[this.direction];
+  var limitX = (this.position.x + CARD_RULE.x * 1 <= area.x) && (this.position.x + CARD_RULE.x * 1 >= 0);
+  var limitY = (this.position.y + CARD_RULE.y * 1 <= area.y) && (this.position.y + CARD_RULE.y * 1 >= 0);
+  var newX = limitX ? this.position.x + CARD_RULE.x * 1 : this.position.x;
+  var newY = limitY ? this.position.y + CARD_RULE.y * 1 : this.position.y;
+
+  if (!limitX || !limitY) {
+    this.direction = CARDINALS_STR[this.prevDirection];      
+  }
+
+  this.position.x = newX;
+  this.position.y = newY;  
+};
+
+var area = new Vector(5,5); // limits
 
 var moves = "GAGAGAGAA";
 var moves2 = "AADAADADDA";
 
+function Play() {    
+  
+  var m1 = new Mower(1,2, 'N');  
+  var m2 = new Mower(3,3, 'E');  
 
-var D = 'D'; // pivot right
-var G = 'G'; // pivot left
-var A = 'A'; // move
-
-
-function Play() {
-  console.log("play");
-
-  var area = Area;
-  area.x = 5;
-  area.y = 5;
-
-  var t1 = Vehicule;
-  t1.x = 1;
-  t1.y = 2;
-  t1.o = 'N';
-
-  var t2 = Vehicule2;
-  t2.x = 3;
-  t2.y = 3;
-  t2.o = 'E';
-
-  var previousPosition = undefined
-
-  var setMove = function(vehicule, flag) {
-    switch (flag) {
-      case 'G':
-        var currentPos = vehicule.o;
-        previousPosition = CARDINALS_STR.indexOf(currentPos);
-        var newIdx = previousPosition - 1 >= 0 ? previousPosition - 1 : CARDINALS_STR.length -1;        
-        vehicule.o = CARDINALS_STR[newIdx];
-        break;
-      case 'D':
-        var currentPos = vehicule.o;
-        previousPosition = CARDINALS_STR.indexOf(currentPos);
-        var newIdx = previousPosition + 1 > 3 ?  0 : previousPosition + 1;        
-        vehicule.o = CARDINALS_STR[newIdx];
-      break;
-      default:
-        doMove(vehicule);
-      break;      
-    }
-  };
-
-  var doMove = function(vehicule) {
-    var CARD_RULE = CARDINALS[CARDINALS_STR.indexOf(vehicule.o)];
-    var limitX = (vehicule.x + CARD_RULE.x * 1 <= area.x) && (vehicule.x + CARD_RULE.x * 1 >= 0);
-    var limitY = (vehicule.y + CARD_RULE.y * 1 <= area.y) && (vehicule.y + CARD_RULE.y * 1 >= 0);
-    var newX = limitX ? vehicule.x + CARD_RULE.x * 1 : vehicule.x;
-    var newY = limitY ? vehicule.y + CARD_RULE.y * 1 : vehicule.y;
-
-    if (!limitX || !limitY) {
-      vehicule.o = CARDINALS_STR[previousPosition];      
-    }
-
-    vehicule.x = newX;
-    vehicule.y = newY;
-
-    console.log(vehicule.x + " " + vehicule.y + vehicule.o)
-  };
+  var previousPosition = undefined  
 
   console.log("***** vehic 1 *******")
-  for (var i = 0; i < moves.length; i++) {
-    var move = moves[i];
-    setMove(t1, move);
-  }
+  console.log(m1.handleCommand(moves));  
 
   console.log("***** vehic 2 *******")
-  for (var i = 0; i < moves2.length; i++) {
-    var move = moves2[i];
-    setMove(t2, move);
-  }
-
- 
+  console.log(m2.handleCommand(moves));  
+  
 }
 
 Play();
+
+var line = 0;    
+rl.on('line', function(line) {
+  if (line === 0) {
+    
+  }  
+})
+.on('error', function(e) {
+  // something went wrong
+});
